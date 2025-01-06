28340d9bb57789ee4ab5ad737a316abc294e14fe
drencys <tiduke390@gmail.com>
2025-01-05 21:26:45 -0800

    commands added

--- PS1 - Disable Defender.ps1
+++ PS1 - Disable Defender.ps1
@@ -1,1 +1,5 @@
-Sample
+# disable
+powershell -command 'Set-MpPreference -DisableRealtimeMonitoring $true -DisableScriptScanning $true -DisableBehaviorMonitoring $true -DisableIOAVProtection $true -DisableIntrusionPreventionSystem $true'
+
+# Or exclude
+powershell -command 'Add-MpPreference -ExclusionPath "c:\temp" -ExclusionProcess "c:\temp\yourstuffs.exe"'
